3293a5e61ef27a2263cd3d1ed02fb2fb1839483b
Antonela Dalmasso Fusco <antodf1@icloud.com>
2026-05-19 12:21:49 -0300

    fix: vuelvo a incluir /api en la url de produccion de axios

--- organizador/frontend/src/api/client.ts
+++ organizador/frontend/src/api/client.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const client = axios.create({
   baseURL: import.meta.env.PROD 
-    ? 'https://devtask-00p5.onrender.com' 
+    ? 'https://devtask-00p5.onrender.com/api' 
     : '/api',
   headers: {
     'Content-Type': 'application/json',
